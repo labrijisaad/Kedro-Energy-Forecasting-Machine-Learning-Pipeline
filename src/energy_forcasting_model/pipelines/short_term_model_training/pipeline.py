2a6f3dacba99c84b8e639faf0b90160c185f4f4e
labri <labrijisaad@gmail.com>
2024-03-28 12:08:04 +0100

    Created the short_term_model_training pipeline skeleton

---
+++ src/energy_forcasting_model/pipelines/short_term_model_training/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'short_term_model_training'
+generated using Kedro 0.19.3
+"""
+
+from kedro.pipeline import Pipeline, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
