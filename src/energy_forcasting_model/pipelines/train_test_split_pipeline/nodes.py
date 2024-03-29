67b847ddde476826cfa5ef97ddd6e4b8b9c9657e
labri <labrijisaad@gmail.com>
2024-03-29 11:40:17 +0100

    Created the train_test_split Pipeline

---
+++ src/energy_forcasting_model/pipelines/train_test_split_pipeline/nodes.py
@@ -0,0 +1,4 @@
+"""
+This is a boilerplate pipeline 'train_test_split_pipeline'
+generated using Kedro 0.19.3
+"""
---
+++ src/energy_forcasting_model/pipelines/train_test_split_pipeline/pipeline.py
@@ -0,0 +1,10 @@
+"""
+This is a boilerplate pipeline 'train_test_split_pipeline'
+generated using Kedro 0.19.3
+"""
+
+from kedro.pipeline import Pipeline, pipeline
+
+
+def create_pipeline(**kwargs) -> Pipeline:
+    return pipeline([])
